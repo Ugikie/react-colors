9c5def882717496a073e99fb086cdcc081f1a9bb
Austin Adam <austinadam42@gmail.com>
2020-12-17 12:58:57 -0800

    Initial Commit
    A bit late on the first commit, however the full palette, single palette, and palette lists are functional.
    
    More to come soon...

---
+++ src/ColorBox.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { Link } from 'react-router-dom';
+import { withStyles } from '@material-ui/styles';
+import styles from './styles/ColorBoxStyles';
+
+class ColorBox extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			copied: false
+		};
+		this.changeCopyState = this.changeCopyState.bind(this);
+	}
+	changeCopyState() {
+		this.setState({ copied: true });
+		setTimeout(() => {
+			this.setState({ copied: false });
+		}, 1500);
+	}
+	render() {
+		const { copied } = this.state;
+		const { name, background, id, paletteId, showingFullPalette, classes } = this.props;
+		return (
+			<CopyToClipboard text={background} onCopy={this.changeCopyState}>
+				<div style={{ background }} className={classes.ColorBox}>
+					<div style={{ background }} className={`${classes.copyOverlay} ${copied && classes.showOverlay}`} />
+					<div className={`${classes.copyMsg} ${copied && classes.showCopyMessage}`}>
+						<h1>Copied!</h1>
+						<p className={classes.copyText}>{background}</p>
+					</div>
+					<div>
+						<div className={classes.boxContent}>
+							<span className={classes.colorName}>{name}</span>
+						</div>
+						<button className={classes.copyButton}>Copy</button>
+					</div>
+					{showingFullPalette && (
+						<Link to={`/palette/${paletteId}/${id}/`} onClick={(e) => e.stopPropagation()}>
+							<span className={classes.seeMore}>MORE</span>
+						</Link>
+					)}
+				</div>
+			</CopyToClipboard>
+		);
+	}
+}
+
+export default withStyles(styles)(ColorBox);
